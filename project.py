8560fd20949cfb6b46270cde987f312e1246cf8c
JulVdev <julvdev@gmail.com>
2018-05-09 18:22:44 +0200

    write project code

--- project.py
+++ project.py
@@ -28,164 +28,106 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 """
-Baxter RSDK Joint Position Waypoints Example
+Baxter RSDK Joint Position Waypoints Tasks
 """
-import argparse
 import sys
-
 import rospy
-
 import baxter_interface
+import json
 
+class BaxterTask(object):
+    def __init__(self, config_file):
+        # Config for the Task
+        self._config = load_file(config_file)
 
-class Waypoints(object):
-    def __init__(self, limb, speed, accuracy):
+        # Put default accuracy if not well specified
+        self.format_accuracy()
+
         # Create baxter_interface limb instance
-        self._arm = limb
+        self._arm = self._config["limb"]
         self._limb = baxter_interface.Limb(self._arm)
 
-        # Parameters which will describe joint position moves
-        self._speed = speed
-        self._accuracy = accuracy
+        # joint angles in time
+        self._angles_list = self.inverse_kinematics()
 
-        # Recorded waypoints
-        self._waypoints = list()
-
-        # Recording state
-        self._is_recording = False
-
-        # Verify robot is enabled
-        print("Getting robot state... ")
-        self._rs = baxter_interface.RobotEnable()
-        self._init_state = self._rs.state().enabled
-        print("Enabling robot... ")
-        self._rs.enable()
-
-        # Create Navigator I/O
-        self._navigator_io = baxter_interface.Navigator(self._arm)
-
-    def _record_waypoint(self, value):
+    def load_file(self, filename):
         """
-        Stores joint position waypoints
-
-        Navigator 'OK/Wheel' button callback
+        Load data from a json file
         """
-        if value:
-            print("Waypoint Recorded")
-            self._waypoints.append(self._limb.joint_angles())
+        with open(filename) as f:
+            data = json.load(f)
+        return data
 
-    def _stop_recording(self, value):
+    def format_accuracy(self):
+        if not isinstance(self._config["accuracy"], (int, float)):
+            self._config["accuracy"] = baxter_interface.settings.JOINT_ANGLE_TOLERANCE
+
+    def inverse_kinematics():
         """
-        Sets is_recording to false
-
-        Navigator 'Rethink' button callback
+        Calculate the inverse kinematics and returns the result
         """
-        # On navigator Rethink button press, stop recording
-        if value:
-            self._is_recording = False
+        # take points from the self._config["waypoints"]
 
-    def record(self):
+    def get_name():
+        return self._config["name"]
+
+    def execute(self):
         """
-        Records joint position waypoints upon each Navigator 'OK/Wheel' button
-        press.
-        """
-        rospy.loginfo("Waypoint Recording Started")
-        print("Press Navigator 'OK/Wheel' button to record a new joint "
-        "joint position waypoint.")
-        print("Press Navigator 'Rethink' button when finished recording "
-              "waypoints to begin playback")
-        # Connect Navigator I/O signals
-        # Navigator scroll wheel button press
-        self._navigator_io.button0_changed.connect(self._record_waypoint)
-        # Navigator Rethink button press
-        self._navigator_io.button2_changed.connect(self._stop_recording)
-
-        # Set recording flag
-        self._is_recording = True
-
-        # Loop until waypoints are done being recorded ('Rethink' Button Press)
-        while self._is_recording and not rospy.is_shutdown():
-            rospy.sleep(1.0)
-
-        # We are now done with the navigator I/O signals, disconnecting them
-        self._navigator_io.button0_changed.disconnect(self._record_waypoint)
-        self._navigator_io.button2_changed.disconnect(self._stop_recording)
-
-    def playback(self):
-        """
-        Loops playback of recorded joint position waypoints until program is
-        exited
+        Execute the task on the robot
         """
         rospy.sleep(1.0)
 
-        rospy.loginfo("Waypoint Playback Started")
-        print("  Press Ctrl-C to stop...")
+        rospy.loginfo("Task Started")
 
         # Set joint position speed ratio for execution
-        self._limb.set_joint_position_speed(self._speed)
+        self._limb.set_joint_position_speed(self._config["speed"])
 
-        # Loop until program is exited
-        loop = 0
-        while not rospy.is_shutdown():
-            loop += 1
-            print("Waypoint playback loop #%d " % (loop,))
-            for waypoint in self._waypoints:
-                if rospy.is_shutdown():
-                    break
-                self._limb.move_to_joint_positions(waypoint, timeout=20.0,
-                                                   threshold=self._accuracy)
-            # Sleep for a few seconds between playback loops
-            rospy.sleep(3.0)
+        # Play the waypoints
+        for angles in self._angles_list:
+            if rospy.is_shutdown():
+                break
+            self._limb.move_to_joint_positions(angles, timeout=20.0,
+                                               threshold=self._config["accuracy"])
+
+        # Sleep for a few seconds
+        rospy.sleep(3.0)
 
         # Set joint position speed back to default
         self._limb.set_joint_position_speed(0.3)
 
-    def clean_shutdown(self):
-        print("\nExiting example...")
-        if not self._init_state:
-            print("Disabling robot...")
-            self._rs.disable()
-        return True
+def enable_robot():
+    print("Getting robot state... ")
+    self._rs = baxter_interface.RobotEnable()
+    self._init_state = self._rs.state().enabled
+    print("Enabling robot... ")
+    self._rs.enable()
 
+def shutdown_robot():
+    print("\nExiting code...")
+    if not self._init_state:
+        print("Disabling robot...")
+        self._rs.disable()
+    return True
 
 def main():
-    """RSDK Joint Position Waypoints Example
+    """RSDK Joint Position Waypoints
 
-    Records joint positions each time the navigator 'OK/wheel'
-    button is pressed.
-    Upon pressing the navigator 'Rethink' button, the recorded joint positions
-    will begin playing back in a loop.
+    Calculate some waypoints from the inverse kinematics then move
     """
-    arg_fmt = argparse.RawDescriptionHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
-                                     description=main.__doc__)
-    required = parser.add_argument_group('required arguments')
-    required.add_argument(
-        '-l', '--limb', required=True, choices=['left', 'right'],
-        help='limb to record/playback waypoints'
-    )
-    parser.add_argument(
-        '-s', '--speed', default=0.3, type=float,
-        help='joint position motion speed ratio [0.0-1.0] (default:= 0.3)'
-    )
-    parser.add_argument(
-        '-a', '--accuracy',
-        default=baxter_interface.settings.JOINT_ANGLE_TOLERANCE, type=float,
-        help='joint position accuracy (rad) at which waypoints must achieve'
-    )
-    args = parser.parse_args(rospy.myargv()[1:])
+
+    enable_robot()
 
     print("Initializing node... ")
-    rospy.init_node("rsdk_joint_position_waypoints_%s" % (args.limb,))
+    rospy.init_node("inverse_kinematics_task")
 
-    waypoints = Waypoints(args.limb, args.speed, args.accuracy)
+    task = BaxterTask("task.json")
 
     # Register clean shutdown
-    rospy.on_shutdown(waypoints.clean_shutdown)
+    rospy.on_shutdown(shutdown_robot)
 
     # Begin example program
-    waypoints.record()
-    waypoints.playback()
+    print("Running task " + task.get_name())
+    task.execute()
 
 if __name__ == '__main__':
     main()
