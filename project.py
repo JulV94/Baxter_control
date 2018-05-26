6510ffacbac3668a7245130c7e1e9fde68d4c587
JulV <julvdev@gmail.com>
2018-05-26 12:41:35 +0200

    refactor code

--- project.py
+++ project.py
@@ -10,6 +10,9 @@ from math import atan2, sqrt, pi
 
 class BaxterTask(object):
     def __init__(self, robot, config_file):
+        """
+        Init BaxterTask object
+        """
         # Store robot instance
         self._robot = robot;
 
@@ -29,10 +32,16 @@ class BaxterTask(object):
         self._waypoints_angles = self.inverse_kinematics(self._config["waypoints"])
 
     def format_accuracy(self):
+        """
+        Change string in config with default value
+        """
         if not isinstance(self._config["accuracy"], (int, float)):
             self._config["accuracy"] = baxter_interface.settings.JOINT_ANGLE_TOLERANCE
 
     def relToAbsolutePosition(self, points):
+        """
+        Convert relative positions to absolute ones
+        """
         for point in points:
             for initpoint in self._config["initpoints"]:
                 if point["limb"] == initpoint["limb"]:
@@ -41,6 +50,9 @@ class BaxterTask(object):
                     point["z"] += initpoint["z"]
 
     def checkAngleLimits(self, angles, point):
+        """
+        Check if angles values are out of bounds and print in logs
+        """
         for i in range(len(angles)):
             if angles[i] > self._config["anglelimits"][i]["max"]:
                 rospy.loginfo("For point (" + str(point["x"]) + "," + str(point["y"]) + "," + str(point["z"]) + ")")
@@ -84,14 +96,22 @@ class BaxterTask(object):
         return results
 
     def get_name(self):
+        """
+        Return task name
+        """
         return self._config["name"]
 
     def waitingToGrip(self):
+        """
+        Waiting for the operator to adjust object in grippers
+        """
         self.gotoPoints(self._grippoints_angles)
         self._robot.gripperControl()
 
     def gotoPoints(self, points):
-        # Play the waypoints
+        """
+        Go to the given points
+        """
         for point in points:
             if rospy.is_shutdown():
                 break
@@ -114,6 +134,7 @@ class BaxterTask(object):
         self.gotoPoints(self._initpoints_angles)
         self._robot.waitForStateChange(2)
         self.gotoPoints(self._waypoints_angles)
+        self._robot.resetState()
 
         # Sleep for a few seconds
         rospy.sleep(3.0)
@@ -124,6 +145,9 @@ class BaxterTask(object):
 
 class BaxterRobot(object):
     def __init__(self):
+        """
+        Init BaxterRobot object
+        """
         # Init the limbs of the robot
         rospy.init_node("inverse_kinematics_task")
         self._limb_left = baxter_interface.Limb("left")
@@ -154,6 +178,9 @@ class BaxterRobot(object):
         self._rs.enable()
 
     def get_limb(self, name):
+        """
+        Get limb object by name
+        """
         if name == "left":
             return self._limb_left
         elif name == "right":
@@ -162,21 +189,36 @@ class BaxterRobot(object):
         exit(1)
 
     def connectButtonState(self):
+        """
+        Connect button to change state of the state machine
+        """
         self._navigator_left.button2_changed.connect(self.incrementState)
         self._navigator_right.button2_changed.connect(self.incrementState)
 
     def disconnectButtonState(self):
+        """
+        Disonnect button to change state of state machine
+        """
         self._navigator_left.button2_changed.disconnect(self.incrementState)
         self._navigator_right.button2_changed.disconnect(self.incrementState)
 
     def incrementState(self, value):
+        """
+        Increment the state machine
+        """
         if value:
             self.state += 1
 
     def resetState(self):
+        """
+        Reset state machine
+        """
         self.state = 0
 
     def gripperControl(self):
+        """
+        Activate gripper control by the wheel
+        """
         self._navigator_left.wheel_changed.connect(self.leftWheelMoved)
         self._navigator_right.wheel_changed.connect(self.rightWheelMoved)
         self.waitForStateChange(1)
@@ -184,6 +226,9 @@ class BaxterRobot(object):
         self._navigator_right.wheel_changed.disconnect(self.rightWheelMoved)
 
     def leftWheelMoved(self, value):
+        """
+        Convert left wheel value to left gripper position
+        """
         self._gripper_left_pos += value * 5
         if self._gripper_left_pos < 0.0:
             self._gripper_left_pos = 0.0
@@ -192,6 +237,9 @@ class BaxterRobot(object):
         self._gripper_left.command_position(self._gripper_left_pos)
 
     def rightWheelMoved(self, value):
+        """
+        Convert right wheel value to right gripper position
+        """
         self._gripper_right_pos += value * 5
         if self._gripper_right_pos < 0.0:
             self._gripper_right_pos = 0.0
@@ -200,12 +248,18 @@ class BaxterRobot(object):
         self._gripper_right.command_position(self._gripper_right_pos)
 
     def waitForStateChange(self, state):
+        """
+        Waits for operator to ask next state
+        """
         self.connectButtonState()
         while (self.state != state) and not rospy.is_shutdown():
             rospy.sleep(1.0)
         self.disconnectButtonState()
 
     def shutdown_robot(self):
+        """
+        Disabling robot on exiting ros
+        """
         rospy.loginfo("\nExiting code...")
         if not self._init_state:
             rospy.loginfo("Disabling robot...")
@@ -213,19 +267,21 @@ class BaxterRobot(object):
         return True
 
 def main():
-    """RSDK Joint Position Waypoints
-
+    """
+    RSDK Joint Position Waypoints
     Calculate some waypoints from the inverse kinematics then move
     """
     print("Initializing node... ")
+    # Init the robot
     robot = BaxterRobot();
-     
+
     # Register clean shutdown
     rospy.on_shutdown(robot.shutdown_robot)
 
+    # Create the task
     task = BaxterTask(robot, "src/baxter_examples/scripts/task.json")
 
-    # Begin example program
+    # Begin the task
     rospy.loginfo("Running task " + task.get_name())
     task.execute()
 
